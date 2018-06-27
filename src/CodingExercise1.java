553e7467ebd156b7cc66347869a1b5fdf5ec38d8
league <league@wts14-4.attlocal.net>
2018-06-26 18:30:02 -0700

    Exam

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -1,19 +1,55 @@
 import java.awt.Color;
+
 import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+	public static void main(String[] args) {
+		// 3. ask the user what color they would like the Robot to draw
 
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
 
-   	 // 2. set the pen width to 10
+		// 2. set the pen width to 10
 
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+		// 1. make the Robot draw a shape
+
+		Robot rob = new Robot();
+		rob.penDown();
+		rob.setSpeed(10);
+
+		String color = JOptionPane.showInputDialog(
+				"Choose a color for the robot to draw.\nRed, Orange, Yellow, Green, Blue, or Magenta.");
+
+		if (color.equalsIgnoreCase("Red")) {
+			rob.setPenColor(Color.RED);
+		} else if (color.equalsIgnoreCase("Orange")) {
+			rob.setPenColor(Color.ORANGE);
+		} else if (color.equalsIgnoreCase("Yellow")) {
+			rob.setPenColor(Color.YELLOW);
+		} else if (color.equalsIgnoreCase("Green")) {
+			rob.setPenColor(Color.GREEN);
+		} else if (color.equalsIgnoreCase("Blue")) {
+			rob.setPenColor(Color.BLUE);
+		} else if (color.equalsIgnoreCase("Purple")) {
+			rob.setPenColor(Color.MAGENTA);
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"Error(s)\n1. That color is not one of the ones the program specified.\n2. You spelled the color incorrectly.\n3. The code is messed up.");
+			System.exit(0);
+		}
+
+		rob.setPenWidth(10);
+
+		for (int i = 0; i < 4; i++) {
+			rob.move(100);
+			rob.turn(90);
+		}
+		rob.hide();
+
+	}
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,17 +1,25 @@
 
-
 /* Level 0 Exam: Coding  Exercise #2 */
 import javax.swing.JOptionPane;
 
 /**
- * Write a program that asks the user for their age. 
- * Tell them which year they were born. 
- * If they are over 30, tell them they are too old to play this game.
+ * Write a program that asks the user for their age. Tell them which year they
+ * were born. If they are over 30, tell them they are too old to play this game.
  **/
 
 public class CodingExercise2 {
 
+	public static void main(String[] args) {
+
+		String x = JOptionPane.showInputDialog("What is your age?");
+
+		int age = Integer.parseInt(x);
+
+		System.out.println("You were born in " + (2018 - age) + ".");
+
+		if (age > 30) {
+			System.out.println("You are too old to play this game!");
+		}
+
+	}
 }
-
-
-
